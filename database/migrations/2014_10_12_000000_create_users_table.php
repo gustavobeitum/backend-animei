0a382e55c68e1b5540ebd17627ef169879927570
Gustavo Beitum <beitmh@gmail.com>
2024-11-22 22:48:46 -0300

    fix: deixar os campos que não são obrigatorio receber valor null

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('image');
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('image')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
