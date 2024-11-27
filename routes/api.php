c85d088edcf0e4e49c71946fe37af2cf51f0cfdc
Gustavo Beitum <beitmh@gmail.com>
2024-11-27 09:02:13 -0300

    feat: adicionar as rotas para login e logout

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,12 +20,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/user', [UserController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function(){
 
-Route::post('/user', [UserController::class, 'store']);
+    Route::put('/user/{id}', [UserController::class, 'update']);
+
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
+    Route::delete('/logout', [AuthenticatedSessionController::class, 'destroy']);
+});
+
+Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/user/{id}', [UserController::class, 'show']);
 
-Route::put('/user/{id}', [UserController::class, 'update']);
+Route::post('/user', [UserController::class, 'store']);
 
-Route::delete('/user/{id}', [UserController::class, 'destroy']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
