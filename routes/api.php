2917dbd370251ae65acd12223ad26e1123dd0226
Gustavo Beitum <beitmh@gmail.com>
2024-12-11 13:29:55 -0300

    feat: definir as rotas de curtir comentarios

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\EmailVerificationWithCodeController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeCommentController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
@@ -32,25 +33,27 @@ Route::middleware('auth:sanctum')->group(function(){
     
     Route::middleware('check')->group(function(){
 
-        Route::delete('/user/{id}', [UserController::class, 'destroy']);
+        Route::delete('/user/{user_id}', [UserController::class, 'destroy']);
 
         Route::post('/post', [PostController::class, 'store']);
-        Route::put('/post/{id}', [PostController::class, 'update']);
-        Route::delete('/post/{id}', [PostController::class, 'destroy']);
+        Route::put('/post/{post_id}', [PostController::class, 'update']);
+        Route::delete('/post/{post_id}', [PostController::class, 'destroy']);
 
         Route::post('/like/{post_id}', [LikeController::class, 'LikePost']);
 
+        Route::post('/like-comment/{comment_id}', [LikeCommentController::class, 'likeComment']);
+
         Route::post('/comment', [CommentController::class, 'store']);
-        Route::put('/comment/{id}', [CommentController::class, 'update']);
-        Route::delete('/comment/{id}', [CommentController::class, 'destroy']);
+        Route::put('/comment/{comment_id}', [CommentController::class, 'update']);
+        Route::delete('/comment/{comment_id}', [CommentController::class, 'destroy']);
 
         
         Route::post('/answer', [AnswerController::class, 'store']);
-        Route::put('/answer/{id}', [AnswerController::class, 'update']);
-        Route::delete('/answer/{id}', [AnswerController::class, 'destroy']);
+        Route::put('/answer/{answer_id}', [AnswerController::class, 'update']);
+        Route::delete('/answer/{answer_id}', [AnswerController::class, 'destroy']);
     });
 
-    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::put('/user/{user_id}', [UserController::class, 'update']);
 
     Route::delete('/logout', [AuthenticatedSessionController::class, 'destroy']);
 
@@ -60,19 +63,21 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('/verification-code-email', [EmailVerificatedController::class, 'verification_email']);
 
     Route::get('/post', [PostController::class, 'index']);
-    Route::get('/post/{id}', [PostController::class, 'show']);
+    Route::get('/post/{post_id}', [PostController::class, 'show']);
 
-    Route::get('/countLikes/{post_id}', [LikeController::class, 'likeCount']);
+    Route::get('/countLikes-post/{post_id}', [LikeController::class, 'likeCount']);
+
+    Route::get('/countLikes-comment/{comment_id}', [LikeCommentController::class, 'likeCountComment']);
 
     Route::get('/comment', [CommentController::class, 'index']);
-    Route::get('/comment/{id}', [CommentController::class, 'show']);
+    Route::get('/comment/{comment_id}', [CommentController::class, 'show']);
 
     Route::get('/answer', [AnswerController::class, 'index']);
-    Route::get('/answer/{id}', [AnswerController::class, 'show']);
+    Route::get('/answer/{answer_id}', [AnswerController::class, 'show']);
 });
 
 Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/{id}', [UserController::class, 'show']);
+Route::get('/user/{user_id}', [UserController::class, 'show']);
 Route::post('/user', [UserController::class, 'store']);
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
